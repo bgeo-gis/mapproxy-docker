fff5a3e0b3b414f6b18e55423098be352500d026
jblanchg <jblanch@bgeo.es>
2025-07-25 11:40:36 +0000

    Update ddl

--- ddl/tiled.sql
+++ ddl/tiled.sql
@@ -31,87 +31,78 @@ CREATE MATERIALIZED VIEW tiled.ud_t_gully AS SELECT * FROM v_edit_gully WHERE st
 -- Create the tilecluster materialized view
 
 -- FOR WS
+
 CREATE MATERIALIZED VIEW tiled.ws_t_tileclusters
 TABLESPACE pg_default
-AS SELECT concat('E', expl_id, '-S', sector_id, '-T', state) AS tilecluster_id,
+AS SELECT concat('E', expl_id, '-T', state) AS tilecluster_id,
     expl_id,
-    sector_id,
     state,
-    st_multi(st_buffer(st_collect(geom), 0.01::double precision)) AS geom
+    st_union(geom) AS geom
    FROM ( SELECT ws_t_node.expl_id,
-            ws_t_node.sector_id,
             ws_t_node.state,
-            st_buffer(st_collect(ws_t_node.the_geom), 30::double precision) AS geom
+            st_collect(st_buffer(ws_t_node.the_geom, 30::double precision)) AS geom
            FROM tiled.ws_t_node
-          GROUP BY ws_t_node.expl_id, ws_t_node.sector_id, ws_t_node.state
+          GROUP BY ws_t_node.expl_id, ws_t_node.state
         UNION
          SELECT ws_t_arc.expl_id,
-            ws_t_arc.sector_id,
             ws_t_arc.state,
-            st_buffer(st_collect(ws_t_arc.the_geom), 30::double precision) AS geom
+            st_collect(st_buffer(ws_t_arc.the_geom, 30::double precision)) AS geom
            FROM tiled.ws_t_arc
-          GROUP BY ws_t_arc.expl_id, ws_t_arc.sector_id, ws_t_arc.state
+          GROUP BY ws_t_arc.expl_id, ws_t_arc.state
         UNION
          SELECT ws_t_connec.expl_id,
-            ws_t_connec.sector_id,
             ws_t_connec.state,
-            st_buffer(st_collect(ws_t_connec.the_geom), 30::double precision) AS geom
+            st_collect(st_buffer(ws_t_connec.the_geom, 30::double precision)) AS geom
            FROM tiled.ws_t_connec
-          GROUP BY ws_t_connec.expl_id, ws_t_connec.sector_id, ws_t_connec.state
+          GROUP BY ws_t_connec.expl_id, ws_t_connec.state
         UNION
          SELECT ws_t_link.expl_id,
-            ws_t_link.sector_id,
             ws_t_link.state,
-            st_buffer(st_collect(ws_t_link.the_geom), 30::double precision, 'endcap=flat join=round'::text) AS geom
+            st_collect(st_buffer(ws_t_link.the_geom, 30::double precision)) AS geom
            FROM tiled.ws_t_link
-          GROUP BY ws_t_link.expl_id, ws_t_link.sector_id, ws_t_link.state) a
-  GROUP BY expl_id, sector_id, state
+          GROUP BY ws_t_link.expl_id, ws_t_link.state) a
+  GROUP BY expl_id, state
 WITH DATA;
 
 -- FOR UD
 CREATE MATERIALIZED VIEW tiled.ud_t_tileclusters
 TABLESPACE pg_default
 AS
-SELECT concat('E', expl_id, '-S', sector_id, '-T', state) AS tilecluster_id,
+SELECT concat('E', expl_id, '-T', state) AS tilecluster_id,
        expl_id,
-       sector_id,
        state,
-       st_multi(st_buffer(st_collect(geom), 0.01::double precision)) AS geom
+       st_union(geom) AS geom
 FROM (
     SELECT ud_t_node.expl_id,
-           ud_t_node.sector_id,
            ud_t_node.state,
-           st_buffer(st_collect(ud_t_node.the_geom), 30::double precision) AS geom
+           st_collect(st_buffer(ud_t_node.the_geom, 30::double precision)) AS geom
     FROM tiled.ud_t_node
-    GROUP BY ud_t_node.expl_id, ud_t_node.sector_id, ud_t_node.state
+    GROUP BY ud_t_node.expl_id, ud_t_node.state
     UNION
     SELECT ud_t_arc.expl_id,
-           ud_t_arc.sector_id,
            ud_t_arc.state,
-           st_buffer(st_collect(ud_t_arc.the_geom), 30::double precision) AS geom
+           st_collect(st_buffer(ud_t_arc.the_geom, 30::double precision)) AS geom
     FROM tiled.ud_t_arc
-    GROUP BY ud_t_arc.expl_id, ud_t_arc.sector_id, ud_t_arc.state
+    GROUP BY ud_t_arc.expl_id, ud_t_arc.state
     UNION
     SELECT ud_t_connec.expl_id,
-           ud_t_connec.sector_id,
            ud_t_connec.state,
-           st_buffer(st_collect(ud_t_connec.the_geom), 30::double precision) AS geom
+           st_collect(st_buffer(ud_t_connec.the_geom, 30::double precision)) AS geom
     FROM tiled.ud_t_connec
-    GROUP BY ud_t_connec.expl_id, ud_t_connec.sector_id, ud_t_connec.state
+    GROUP BY ud_t_connec.expl_id, ud_t_connec.state
     UNION
     SELECT ud_t_link.expl_id,
-           ud_t_link.sector_id,
            ud_t_link.state,
-           st_buffer(st_collect(ud_t_link.the_geom), 30::double precision, 'endcap=flat join=round'::text) AS geom
+           st_collect(st_buffer(ud_t_link.the_geom, 30::double precision)) AS geom
     FROM tiled.ud_t_link
-    GROUP BY ud_t_link.expl_id, ud_t_link.sector_id, ud_t_link.state
+    GROUP BY ud_t_link.expl_id, ud_t_link.state
     UNION
     SELECT ud_t_gully.expl_id,
            ud_t_gully.sector_id,
            ud_t_gully.state,
-           st_buffer(st_collect(ud_t_gully.the_geom), 30::double precision) AS geom
+           st_collect(st_buffer(ud_t_gully.the_geom, 30::double precision)) AS geom
     FROM tiled.ud_t_gully
     GROUP BY ud_t_gully.expl_id, ud_t_gully.sector_id, ud_t_gully.state
 ) a
-GROUP BY expl_id, sector_id, state
+GROUP BY expl_id, state, sector_id
 WITH DATA;
